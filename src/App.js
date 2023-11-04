5715b431915f394d90c352c24b39b62609a85687
Njokimary <marynjoki738@gmail.com>
2023-11-04 16:43:20 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import RestaurantList from './components/RestaurantList';
+import RestaurantDetail from './components/RestaurantDetail';
+import CreateRestaurantPizza from './components/CreateRestaurantPizza';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/restaurants/:id" element={<RestaurantDetail />} />
+        <Route path="/create-restaurant-pizza" element={<CreateRestaurantPizza />} />
+        <Route path="/" element={<RestaurantList />} />
+      </Routes>
+    </Router>
   );
 }
 
